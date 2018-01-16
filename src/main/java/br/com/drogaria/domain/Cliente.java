44e8c16991ef8dae8649d72a047a0537da0c1f4d
Andre Marinho <andre.bmarinho@hotmail.com>
2018-01-16 06:47:40 -0300

    Entidade Cliente, Funcionario, Pessoa

---
+++ src/main/java/br/com/drogaria/domain/Cliente.java
@@ -0,0 +1,53 @@
+package br.com.drogaria.domain;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@SuppressWarnings("serial")
+@Entity
+public class Cliente extends GenericDomain {
+
+	@Column(nullable = false)
+	@Temporal(TemporalType.DATE)
+	private Date dataCadastro;
+
+	@Column(nullable = false)
+	private boolean liberado;
+
+	@OneToOne
+	@JoinColumn(nullable = false)
+	private Pessoa pessoa;
+
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public boolean isLiberado() {
+		return liberado;
+	}
+
+	public void setLiberado(boolean liberado) {
+		this.liberado = liberado;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+	
+	
+
+}
