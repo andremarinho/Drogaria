1d8a301eb62d7c4832e93b60da51b4c244205ee6
Andre Marinho <andre.bmarinho@hotmail.com>
2018-01-31 07:17:22 -0300

    Implementação do Omnifaces

--- src/main/java/br/com/drogaria/bean/EstadoBean.java
+++ src/main/java/br/com/drogaria/bean/EstadoBean.java
@@ -1,17 +1,17 @@
 package br.com.drogaria.bean;
 
-import javax.faces.application.FacesMessage;
+
 import javax.faces.bean.ManagedBean;
-import javax.faces.context.FacesContext;
+
+import org.omnifaces.util.Messages;
+import org.omnifaces.util.Messages.Message;
+
 
 @ManagedBean
 public class EstadoBean {
 
 	public void salvar() {
-		FacesMessage messagem = new FacesMessage(FacesMessage.SEVERITY_INFO, "Programação Web com Java",
-				"Programação Web com Java");
-		FacesContext contexto = FacesContext.getCurrentInstance(); 
-		contexto.addMessage(null, messagem);
+		Messages.addGlobalInfo("Programação Web Java");
 	}
 
 }
