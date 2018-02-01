d8da9e2564c6db361a654fc364eee273694d48ff
Andre Marinho <andre.bmarinho@hotmail.com>
2018-02-01 07:10:18 -0300

    Implementando metodo merge no estadoBean

--- src/main/java/br/com/drogaria/bean/EstadoBean.java
+++ src/main/java/br/com/drogaria/bean/EstadoBean.java
@@ -6,9 +6,28 @@ import javax.faces.bean.ManagedBean;
 import org.omnifaces.util.Messages;
 import org.omnifaces.util.Messages.Message;
 
+import br.com.drogaria.domain.Estado;
+
 
 @ManagedBean
 public class EstadoBean {
+
+	
+	private Estado estado;
+	
+	
+	
+	public Estado getEstado() {
+		return estado;
+	}
+
+
+
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+
+
 
 	public void salvar() {
 		Messages.addGlobalInfo("Programação Web Java");
--- src/test/java/br/com/drograria/dao/FabricanteDAOTest.java
+++ src/test/java/br/com/drograria/dao/FabricanteDAOTest.java
@@ -1,5 +1,6 @@
 package br.com.drograria.dao;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import br.com.drogaria.dao.FabricanteDAO;
@@ -8,6 +9,7 @@ import br.com.drogaria.domain.Fabricante;
 public class FabricanteDAOTest {
 	
 	@Test
+	@Ignore
 	public void salvar(){
 		Fabricante fabricante = new Fabricante();
 		fabricante.setDescricao("AMS");
@@ -15,5 +17,18 @@ public class FabricanteDAOTest {
 		FabricanteDAO fabricanteDAO = new FabricanteDAO();
 		fabricanteDAO.salvar(fabricante);
 	}
+	
+	
+	@Test
+	public void merge(){
+		Fabricante fabricante = new Fabricante();
+		
+		
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		fabricante = fabricanteDAO.buscar(2L);
+		fabricante.setDescricao("Fabricante A alterado");
+		fabricanteDAO.merge(fabricante);
+	}
+	
 
 }
