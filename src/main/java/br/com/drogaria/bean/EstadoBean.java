5f09a8b12289e41f15684db44843c9d4a8672607
Andre Marinho <andre.bmarinho@hotmail.com>
2018-02-04 09:24:33 -0300

    Implemantado metodo excluir

--- src/main/java/br/com/drogaria/bean/EstadoBean.java
+++ src/main/java/br/com/drogaria/bean/EstadoBean.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
 import javax.management.RuntimeErrorException;
 
 import org.omnifaces.util.Messages;
@@ -25,7 +26,7 @@ public class EstadoBean implements Serializable {
 
 		return estado;
 	}
-	
+
 	public List<Estado> getEstados() {
 		return estados;
 	}
@@ -33,36 +34,36 @@ public class EstadoBean implements Serializable {
 	public void setEstado(Estado estado) {
 		this.estado = estado;
 	}
-	
+
 	public void setEstados(List<Estado> estados) {
 		this.estados = estados;
 	}
 
 	public void salvar() {
-		
+
 		try {
 			EstadoDAO estadoDAO = new EstadoDAO();
 			estadoDAO.salvar(estado);
-			
+
 			Messages.addGlobalInfo("Estado salvo com sucesso!!!");
 			novo();
-			throw new RuntimeErrorException(null, "erro forçado");
-			
+			this.listar();
+
 		} catch (RuntimeException e) {
 			Messages.addError(null, "Aconteceu erro ao tentar salvar", null);
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	@PostConstruct
-	public void listar(){
-		
+	public void listar() {
+
 		try {
-			
+
 			EstadoDAO estadoDAO = new EstadoDAO();
 			this.estados = estadoDAO.listar();
-			
+
 		} catch (Exception e) {
 			Messages.addError(null, "Aconteceu um erro ao tentar acessar o banco", null);
 		}
@@ -70,6 +71,20 @@ public class EstadoBean implements Serializable {
 
 	public void novo() {
 		this.estado = new Estado();
+	}
+
+	public void excluir(ActionEvent actionEvent) {
+		this.estado = (Estado) actionEvent.getComponent().getAttributes().get("estadoSelecionado");
+				
+		try {
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estadoDAO.excluir(estado);
+			this.listar();
+			Messages.addGlobalInfo("Estado removido com sucesso");
+		} catch (Exception e) {
+			Messages.addGlobalError("Não é possivel deletar o estado, erro:  "+e.getMessage());
+			e.printStackTrace();
+		}
 	}
 
 }
