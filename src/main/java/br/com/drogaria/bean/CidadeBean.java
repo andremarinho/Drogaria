048805cf99c4606d2c3bc06d349b2d483656b656
Andre Marinho <andre.bmarinho@hotmail.com>
2018-02-08 06:30:46 -0300

    implementando selectOneMenu do estado

--- src/main/java/br/com/drogaria/bean/CidadeBean.java
+++ src/main/java/br/com/drogaria/bean/CidadeBean.java
@@ -10,35 +10,40 @@ import javax.faces.bean.ViewScoped;
 import org.omnifaces.util.Messages;
 
 import br.com.drogaria.dao.CidadeDAO;
+import br.com.drogaria.dao.EstadoDAO;
 import br.com.drogaria.domain.Cidade;
+import br.com.drogaria.domain.Estado;
 
 @SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped
-public class CidadeBean implements Serializable{
-	
+public class CidadeBean implements Serializable {
+
 	private Cidade cidade;
 
 	private List<Cidade> cidades;
-	
+	private List<Estado> estados;
+
 	@PostConstruct
-	public void init(){
+	public void init() {
 		this.listar();
 	}
 
-	
-	
+	public List<Estado> getEstados() {
+		return estados;
+	}
+
+	public void setEstados(List<Estado> estados) {
+		this.estados = estados;
+	}
+
 	public Cidade getCidade() {
 		return cidade;
 	}
 
-
-
 	public void setCidade(Cidade cidade) {
 		this.cidade = cidade;
 	}
-
-
 
 	public List<Cidade> getCidades() {
 		return cidades;
@@ -47,21 +52,42 @@ public class CidadeBean implements Serializable{
 	public void setCidades(List<Cidade> cidades) {
 		this.cidades = cidades;
 	}
-	
-	public void listar(){
+
+	public void listar() {
 		try {
-			
+
 			CidadeDAO cidadeDAO = new CidadeDAO();
 			this.cidades = cidadeDAO.listar();
-			
+
 		} catch (RuntimeException erro) {
 			Messages.addFlashGlobalError("Ocorreu um erro ao tentar listar as cidades");
 			erro.printStackTrace();
 		}
 	}
-	
-	public void novo(){
+
+	public void novo() {
 		this.cidade = new Cidade();
+
+		try {
+			EstadoDAO estadoDAO = new EstadoDAO();
+			this.estados = estadoDAO.listar();
+		} catch (RuntimeException erro) {
+			Messages.addFlashGlobalError("Ocorreu um erro ao tentar listar os estados");
+			erro.printStackTrace();
+		}
 	}
 	
+	public void salvar(){
+		
+		try {
+			
+			Messages.addGlobalInfo("Cidade Salva com sucesso!");
+		} catch (RuntimeException erro) {
+			Messages.addFlashGlobalError("Ocorreu um erro ao tentar salvar uma cidade");
+			erro.printStackTrace();
+		}
+		
+		
+	}
+
 }
