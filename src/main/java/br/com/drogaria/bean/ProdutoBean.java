5e649a81515f28f792239b7cc21b91c35e703236
Andre Marinho <andre.bmarinho@hotmail.com>
2018-02-12 12:44:59 -0300

    Validação da tela de cadastro de produtos

---
+++ src/main/java/br/com/drogaria/bean/ProdutoBean.java
@@ -0,0 +1,110 @@
+package br.com.drogaria.bean;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+
+import org.omnifaces.util.Messages;
+
+import br.com.drogaria.dao.EstadoDAO;
+import br.com.drogaria.dao.FabricanteDAO;
+import br.com.drogaria.dao.ProdutoDAO;
+import br.com.drogaria.domain.Fabricante;
+import br.com.drogaria.domain.Produto;
+
+
+@ManagedBean
+public class ProdutoBean {
+
+	private List<Fabricante> fabricantes;
+	private List<Produto> produtos;
+	private Produto produto;
+	
+	@PostConstruct
+	public void init(){
+		this.listarProdutos();
+	}
+	
+	
+	
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+
+
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
+
+
+
+	public Produto getProduto() {
+		if(produto == null){
+			this.produto = new Produto();
+		}
+		return produto;
+	}
+
+
+
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+
+
+
+	public List<Fabricante> getFabricantes() {
+		if(this.fabricantes == null){
+			this.fabricantes = this.listarFabricantes();
+		}
+		
+		return fabricantes;
+	}
+
+
+
+
+	public void setFabricantes(List<Fabricante> fabricantes) {
+		this.fabricantes = fabricantes;
+	}
+
+
+
+
+	public List<Fabricante> listarFabricantes(){
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		this.fabricantes = fabricanteDAO.listar();
+		return this.fabricantes;
+	}
+	
+	
+	public List<Produto> listarProdutos(){
+		ProdutoDAO produtoDAO = new ProdutoDAO();
+		this.produtos = produtoDAO.listar();
+		return produtos;
+	}
+	
+	public void salvar(){
+		try {
+			ProdutoDAO produtoDAO = new ProdutoDAO();
+			produtoDAO.merge(produto);
+			this.listarProdutos();
+
+			Messages.addGlobalInfo("Produto salvo com sucesso!!!");
+			
+			
+
+		} catch (RuntimeException e) {
+			Messages.addError(null, "Aconteceu erro ao tentar salvar", null);
+			e.printStackTrace();
+		}
+	}
+	
+}
--- src/test/java/br/com/drograria/dao/FabricanteDAOTest.java
+++ src/test/java/br/com/drograria/dao/FabricanteDAOTest.java
@@ -7,28 +7,26 @@ import br.com.drogaria.dao.FabricanteDAO;
 import br.com.drogaria.domain.Fabricante;
 
 public class FabricanteDAOTest {
-	
+
 	@Test
 	@Ignore
-	public void salvar(){
+	public void salvar() {
 		Fabricante fabricante = new Fabricante();
 		fabricante.setDescricao("AMS");
-		
+
 		FabricanteDAO fabricanteDAO = new FabricanteDAO();
 		fabricanteDAO.salvar(fabricante);
 	}
-	
-	
+
 	@Test
-	public void merge(){
+	@Ignore
+	public void merge() {
 		Fabricante fabricante = new Fabricante();
-		
-		
+
 		FabricanteDAO fabricanteDAO = new FabricanteDAO();
 		fabricante = fabricanteDAO.buscar(2L);
 		fabricante.setDescricao("Fabricante A alterado");
 		fabricanteDAO.merge(fabricante);
 	}
-	
 
 }
