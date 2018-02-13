a6f3969ff219b50e07167be4beb11e70ea51a223
Andre Marinho <andre.bmarinho@hotmail.com>
2018-02-13 10:29:10 -0300

    Implementação de validação de quantidade e preço na tela de produtos

--- src/main/java/br/com/drogaria/bean/ProdutoBean.java
+++ src/main/java/br/com/drogaria/bean/ProdutoBean.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.event.ActionEvent;
 
 import org.omnifaces.util.Messages;
 
@@ -13,98 +14,85 @@ import br.com.drogaria.dao.ProdutoDAO;
 import br.com.drogaria.domain.Fabricante;
 import br.com.drogaria.domain.Produto;
 
-
 @ManagedBean
 public class ProdutoBean {
 
 	private List<Fabricante> fabricantes;
 	private List<Produto> produtos;
 	private Produto produto;
-	
+
 	@PostConstruct
-	public void init(){
+	public void init() {
 		this.listarProdutos();
 	}
-	
-	
-	
-	
+
 	public List<Produto> getProdutos() {
 		return produtos;
 	}
-
-
-
 
 	public void setProdutos(List<Produto> produtos) {
 		this.produtos = produtos;
 	}
 
-
-
-
 	public Produto getProduto() {
-		if(produto == null){
+		if (produto == null) {
 			this.produto = new Produto();
 		}
 		return produto;
 	}
 
-
-
-
 	public void setProduto(Produto produto) {
 		this.produto = produto;
 	}
 
-
-
-
 	public List<Fabricante> getFabricantes() {
-		if(this.fabricantes == null){
+		if (this.fabricantes == null) {
 			this.fabricantes = this.listarFabricantes();
 		}
-		
+
 		return fabricantes;
 	}
-
-
-
 
 	public void setFabricantes(List<Fabricante> fabricantes) {
 		this.fabricantes = fabricantes;
 	}
 
-
-
-
-	public List<Fabricante> listarFabricantes(){
+	public List<Fabricante> listarFabricantes() {
 		FabricanteDAO fabricanteDAO = new FabricanteDAO();
 		this.fabricantes = fabricanteDAO.listar();
 		return this.fabricantes;
 	}
-	
-	
-	public List<Produto> listarProdutos(){
+
+	public List<Produto> listarProdutos() {
 		ProdutoDAO produtoDAO = new ProdutoDAO();
 		this.produtos = produtoDAO.listar();
 		return produtos;
 	}
-	
-	public void salvar(){
+
+	public void salvar() {
 		try {
 			ProdutoDAO produtoDAO = new ProdutoDAO();
 			produtoDAO.merge(produto);
 			this.listarProdutos();
 
 			Messages.addGlobalInfo("Produto salvo com sucesso!!!");
-			
-			
 
 		} catch (RuntimeException e) {
 			Messages.addError(null, "Aconteceu erro ao tentar salvar", null);
 			e.printStackTrace();
 		}
 	}
-	
+
+	public void editar(ActionEvent evento) {
+		try {
+			produto = (Produto) evento.getComponent().getAttributes().get("produtoSelecionado");
+
+			FabricanteDAO fabricanteDAO = new FabricanteDAO();
+			fabricantes = fabricanteDAO.listar();
+		} catch (RuntimeException erro) {
+			Messages.addFlashGlobalError("Ocorreu um erro ao tentar selecionar um produto");
+			erro.printStackTrace();
+		}
+	}
+
 }
