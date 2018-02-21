921a8ed3f919455befb3d53376eff1ba0b39360b
Andre Marinho <andre.bmarinho@hotmail.com>
2018-02-21 07:19:22 -0300

    Implamentação da tela de pessoas

---
+++ src/main/java/br/com/drogaria/bean/PessoaBean.java
@@ -0,0 +1,105 @@
+package br.com.drogaria.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
+
+import org.omnifaces.util.Messages;
+
+import br.com.drogaria.dao.PessoaDAO;
+import br.com.drogaria.domain.Cidade;
+import br.com.drogaria.domain.Estado;
+import br.com.drogaria.domain.Pessoa;
+
+
+
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+public class PessoaBean implements Serializable {
+	private Pessoa pessoa;
+	private List<Pessoa> pessoas;
+	
+	private List<Estado> estados;
+	private List<Cidade> cidades;
+	
+	
+	
+	
+	public Pessoa getPessoa() {
+		if(this.pessoa == null ){
+			this.pessoa = new Pessoa();
+		}
+		
+		return pessoa;
+	}
+	
+	
+	
+	public List<Estado> getEstados() {
+		return estados;
+	}
+
+
+
+	public void setEstados(List<Estado> estados) {
+		this.estados = estados;
+	}
+
+
+
+	public List<Cidade> getCidades() {
+		return cidades;
+	}
+
+
+
+	public void setCidades(List<Cidade> cidades) {
+		this.cidades = cidades;
+	}
+
+
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+	
+	public List<Pessoa> getPessoas() {
+		return pessoas;
+	}
+	
+	public void setPessoas(List<Pessoa> pessoas) {
+		this.pessoas = pessoas;
+	}
+	
+	@PostConstruct
+	public void listar() {
+		try{
+			PessoaDAO pessoaDAO = new PessoaDAO();
+			pessoas = pessoaDAO.listar();
+		}catch(RuntimeException erro){
+			Messages.addGlobalError("Ocorreu um erro ao tentar listar as pessoas");
+			erro.printStackTrace();
+		}
+	}
+
+	public void novo() {
+
+	}
+
+	public void editar(ActionEvent evento) {
+
+	}
+
+	public void salvar() {
+
+	}
+
+	public void excluir(ActionEvent evento) {
+
+	}
+}
