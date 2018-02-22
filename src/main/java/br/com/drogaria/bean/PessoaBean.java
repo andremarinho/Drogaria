26311e5bc7e462e1cde249881ddead590d553d6f
Andre Marinho <andre.bmarinho@hotmail.com>
2018-02-22 07:22:45 -0300

    Implementando pessoaBean novo

--- src/main/java/br/com/drogaria/bean/PessoaBean.java
+++ src/main/java/br/com/drogaria/bean/PessoaBean.java
@@ -10,12 +10,11 @@ import javax.faces.event.ActionEvent;
 
 import org.omnifaces.util.Messages;
 
+import br.com.drogaria.dao.EstadoDAO;
 import br.com.drogaria.dao.PessoaDAO;
 import br.com.drogaria.domain.Cidade;
 import br.com.drogaria.domain.Estado;
 import br.com.drogaria.domain.Pessoa;
-
-
 
 @SuppressWarnings("serial")
 @ManagedBean
@@ -23,71 +22,67 @@ import br.com.drogaria.domain.Pessoa;
 public class PessoaBean implements Serializable {
 	private Pessoa pessoa;
 	private List<Pessoa> pessoas;
-	
+
 	private List<Estado> estados;
 	private List<Cidade> cidades;
-	
-	
-	
-	
+
 	public Pessoa getPessoa() {
-		if(this.pessoa == null ){
-			this.pessoa = new Pessoa();
-		}
-		
+
 		return pessoa;
 	}
-	
-	
-	
+
 	public List<Estado> getEstados() {
 		return estados;
 	}
-
-
 
 	public void setEstados(List<Estado> estados) {
 		this.estados = estados;
 	}
 
-
-
 	public List<Cidade> getCidades() {
 		return cidades;
 	}
-
-
 
 	public void setCidades(List<Cidade> cidades) {
 		this.cidades = cidades;
 	}
 
-
-
 	public void setPessoa(Pessoa pessoa) {
 		this.pessoa = pessoa;
 	}
-	
+
 	public List<Pessoa> getPessoas() {
 		return pessoas;
 	}
-	
+
 	public void setPessoas(List<Pessoa> pessoas) {
 		this.pessoas = pessoas;
 	}
-	
+
 	@PostConstruct
 	public void listar() {
-		try{
+		try {
 			PessoaDAO pessoaDAO = new PessoaDAO();
 			pessoas = pessoaDAO.listar();
-		}catch(RuntimeException erro){
+		} catch (RuntimeException erro) {
 			Messages.addGlobalError("Ocorreu um erro ao tentar listar as pessoas");
 			erro.printStackTrace();
 		}
 	}
 
 	public void novo() {
+
+		try {
+
+			this.pessoa = new Pessoa();
+
+			EstadoDAO estadoDAO = new EstadoDAO();
+			this.estados = estadoDAO.listar();
+
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar gerar uma nova pessoa");
+			erro.printStackTrace();
+		}
 
 	}
 
