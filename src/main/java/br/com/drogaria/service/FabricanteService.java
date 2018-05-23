117cf879d3d6c61368e7b302f201a862a4e7a405
Andre Marinho <andre.bmarinho@hotmail.com>
2018-05-23 06:21:34 -0300

    Implementação do Gson, metodo listar

---
+++ src/main/java/br/com/drogaria/service/FabricanteService.java
@@ -0,0 +1,27 @@
+package br.com.drogaria.service;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import com.google.gson.Gson;
+
+import br.com.drogaria.dao.FabricanteDAO;
+import br.com.drogaria.domain.Fabricante;
+
+@Path("fabricante")
+public class FabricanteService {
+	
+	@GET
+	public String listar(){
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		List<Fabricante> fabricantes = fabricanteDAO.listar("descricao");
+		
+		Gson gson = new Gson();
+		String json = gson.toJson(fabricantes);
+		
+		return json;
+	}
+
+}
