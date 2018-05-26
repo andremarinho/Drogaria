0a19b4c9d554dcf579dddb99f2e077c70c7994b8
Andre Marinho <andre.bmarinho@hotmail.com>
2018-05-25 21:07:51 -0300

    Implementado FabricanteService - Editar

--- src/main/java/br/com/drogaria/service/FabricanteService.java
+++ src/main/java/br/com/drogaria/service/FabricanteService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
@@ -14,44 +15,53 @@ import br.com.drogaria.domain.Fabricante;
 
 @Path("fabricante")
 public class FabricanteService {
-	
+
 	@GET
-	public String listar(){
+	public String listar() {
 		FabricanteDAO fabricanteDAO = new FabricanteDAO();
 		List<Fabricante> fabricantes = fabricanteDAO.listar("descricao");
-		
+
 		Gson gson = new Gson();
 		String json = gson.toJson(fabricantes);
-		
+
 		return json;
 	}
-	
-	
+
 	@GET
 	@Path("{codigo}")
-	public String buscar(@PathParam("codigo") Long codigo){
+	public String buscar(@PathParam("codigo") Long codigo) {
 		FabricanteDAO fabricanteDAO = new FabricanteDAO();
 		Fabricante fabricante = fabricanteDAO.buscar(codigo);
-		
+
 		Gson gson = new Gson();
 		String json = gson.toJson(fabricante);
 		return json;
 	}
-	
-	
+
 	@POST
-	public String salvar(String json){
-		
+	public String salvar(String json) {
+
 		Gson gson = new Gson();
 		Fabricante fabricante = gson.fromJson(json, Fabricante.class);
 		FabricanteDAO fabricanteDAO = new FabricanteDAO();
 		fabricanteDAO.merge(fabricante);
-		
+
 		String jsonSaida = gson.toJson(fabricante);
 		return jsonSaida;
-		
-		
-		
+
+	}
+
+	@PUT
+	public String editar(String json) {
+
+		Gson gson = new Gson();
+		Fabricante fabricante = gson.fromJson(json, Fabricante.class);
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		fabricanteDAO.merge(fabricante);
+
+		String jsonSaida = gson.toJson(fabricante);
+		return jsonSaida;
+
 	}
 
 }
